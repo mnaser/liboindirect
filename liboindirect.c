835f072876bc0e460f69b6b2067ff0f21a62d50e
Mohammed Naser <mnaser@vexxhost.com>
2013-05-03 16:33:51 -0300

    Update liboindirect.c

--- liboindirect.c
+++ liboindirect.c
@@ -13,7 +13,6 @@
 #include <fcntl.h>
 #include <ctype.h>
 
-
 #define __cons __attribute__((constructor))
 
 #define TRAP(func, params)                                              \
@@ -26,118 +25,32 @@
         int evil_##func params
 
 
-int loi_debug = 0;
-
-int
-dbg (const char *fmt, ...)
-{
-        int     ret = 0;
-        va_list ap;
-
-        if (!loi_debug)
-                return 0;
-
-        va_start (ap, fmt);
-        ret = vfprintf (stderr, fmt, ap);
-        va_end (ap);
-
-        return ret;
-}
-
-
-int
-err (const char *fmt, ...)
-{
-        int     ret = 0;
-        va_list ap;
-
-        va_start (ap, fmt);
-        ret = vfprintf (stderr, fmt, ap);
-        va_end (ap);
-
-        return ret;
-}
-
-
-static void
-dbg_init ()
-{
-        if (getenv ("LOI_DEBUG"))
-                loi_debug = 1;
-}
-
-
-
 int
 TRAP (open, (const char *path, int flags, mode_t mode))
 {
-        int ret;
-
-        ret = real_open (path, flags, mode);
-
-        if (ret >= 0)
-                return ret;
-
-        if (ret == -1 && errno == EINVAL && (flags & O_DIRECT)) {
+        if (flags & O_DIRECT) 
                 flags &= ~O_DIRECT;
-        }
 
-        ret = real_open (path, flags, mode);
-
-        return ret;
+        return real_open (path, flags, mode);
 }
 
 
 int
 TRAP (open64, (const char *path, int flags, mode_t mode))
 {
-        int ret;
-
-        ret = real_open64 (path, flags, mode);
-
-        if (ret >= 0)
-                return ret;
-
-        if (ret == -1 && errno == EINVAL && (flags & O_DIRECT)) {
+        if (flags & O_DIRECT) 
                 flags &= ~O_DIRECT;
-        }
 
-        ret = real_open64 (path, flags, mode);
-
-        return ret;
+        return real_open64 (path, flags, mode);
 }
 
 
 int
 TRAP (openat, (int dirfd, const char *path, int flags, mode_t mode))
 {
-        int ret;
-
-        ret = real_openat (dirfd, path, flags, mode);
-
-        if (ret >= 0)
-                return ret;
-
-        if (ret == -1 && errno == EINVAL && (flags & O_DIRECT)) {
+        if (flags & O_DIRECT) 
                 flags &= ~O_DIRECT;
-        }
 
-        ret = real_openat (dirfd, path, flags, mode);
-
-        return ret;
+        return real_openat (dirfd, path, flags, mode);
 
 }
-
-
-
-static void loi_init (void) __attribute__((constructor));
-
-
-static void
-loi_init (void)
-{
-        dbg_init ();
-
-        return;
-}
-
